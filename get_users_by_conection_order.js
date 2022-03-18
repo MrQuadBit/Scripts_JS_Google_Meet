9aa9de154ff3eab20c1b475b5e88e44d7e9b6071
Dano <56614565+MrQuadBit@users.noreply.github.com>
2022-03-18 11:22:55 -0600

    Update get_users_by_conection_order.js

--- get_users_by_conection_order.js
+++ get_users_by_conection_order.js
@@ -4,14 +4,18 @@ let size_participants = 0
 while(1){
 	//"zWGUib" is the class for getting the names
 	let participants = document.getElementsByClassName("zWGUib")
+	let new_participants = 0
 
 	for(let name of participants){
 		if(! participants_in_order.includes(name.textContent)){
 			participants_in_order.push(name.textContent)
-			size_participants ++
+			new_participants ++
 		}
 	}
 
-	if(size_participants != participants_in_order.length)
-	console.log(participants_in_order)
+	if(size_participants != participants_in_order.length){
+		console.log(participants_in_order)
+		size_participants += new_participants
+	}
+	
 }
