89a5e49344b06da54ed278a99beb1690a5177985
Dano <56614565+MrQuadBit@users.noreply.github.com>
2022-03-22 08:38:12 -0600

    Update get_users_by_connection_order.js

--- get_users_by_connection_order.js
+++ get_users_by_connection_order.js
@@ -40,7 +40,11 @@ const container = document.getElementsByClassName(CONTAINER_NEW_BUTTON)[0]
 let new_button = document.createElement("button")
 new_button.innerHTML = escapeHTMLPolicy.createHTML("Pasar Lista")
 new_button.onclick = function() {
-	navigator.clipboard.writeText(participants_in_order)
+	let participants = ""
+	for(let i = 0; i < participants_in_order.length; i++){
+		participants = participants.concat(i+1, " - ", participants_in_order[i], "\n")
+	}
+	navigator.clipboard.writeText(participants)
 	alert("Participantes copiados en el portapapeles")
 }
 container.appendChild(new_button)
