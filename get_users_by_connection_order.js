917d18511ca72a88ea4ce58cc6ba066db7a4d45d
Dano <56614565+MrQuadBit@users.noreply.github.com>
2022-03-22 08:21:25 -0600

    Update get_users_by_connection_order.js

--- get_users_by_connection_order.js
+++ get_users_by_connection_order.js
@@ -1,21 +1,46 @@
 let participants_in_order = []
-let size_participants = 0
-
-while(1){
-	//"zWGUib" is the class for getting the names
-	let participants = document.getElementsByClassName("zWGUib")
+//Test
+let original_size = 0 //Aux to know if the array changes
+//Class identifiers in google meet
+const CONTAINER_PARTICIPANTS = "zWGUib"
+const CONTAINER_NEW_BUTTON = "vqs9je"
+//-------------------------------------------------------
+function check_new_users(){
+	let participants = document.getElementsByClassName(CONTAINER_PARTICIPANTS)
 	let new_participants = 0
 
+	//if there is a new participant add it at the end
 	for(let name of participants){
 		if(! participants_in_order.includes(name.textContent)){
 			participants_in_order.push(name.textContent)
+			//Test
 			new_participants ++
 		}
 	}
 
-	if(size_participants != participants_in_order.length){
+	//Test
+	//Print when the list changes
+	if(participants_in_order.length > original_size){
 		console.log(participants_in_order)
-		size_participants += new_participants
+		original_size += new_participants
+	}else{
+		console.log("No new users")
 	}
 	
 }
+let run_checking = window.setInterval(check_new_users, 1000)
+function stop_checking(){
+	clearInterval(run_checking)
+}
+//-------------------------------------------------------
+let escapeHTMLPolicy = trustedTypes.createPolicy("forceInner", {
+	createHTML: (to_escape) => to_escape
+})
+const container = document.getElementsByClassName(CONTAINER_NEW_BUTTON)[0]
+let new_button = document.createElement("button")
+new_button.innerHTML = escapeHTMLPolicy.createHTML("Pasar Lista")
+new_button.onclick = function() {
+	navigator.clipboard.writeText(participants_in_order)
+	alert("Participantes copiados en el portapapeles")
+}
+container.appendChild(new_button)
