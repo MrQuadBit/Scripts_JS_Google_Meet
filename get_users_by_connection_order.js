aef76d05f8605238c4f981b268bcd97f3527f02a
Dano <56614565+MrQuadBit@users.noreply.github.com>
2022-03-24 09:43:59 -0600

    Update get_users_by_connection_order.js

--- get_users_by_connection_order.js
+++ get_users_by_connection_order.js
@@ -1,34 +1,52 @@
-let participants_in_order = []
-//Test
-let original_size = 0 //Aux to know if the array changes
+let team_members = []
+let aplc_members = []
 //Class identifiers in google meet
 const CONTAINER_PARTICIPANTS = "zWGUib"
-const CONTAINER_NEW_BUTTON = "vqs9je"
-//-------------------------------------------------------
+const CONTAINER_NEW_BUTTON = "R5ccN" // This change all the time, so review it if it give an error
+const TIME_REPLAY = 550
+
+//ToDo: Add team leader to the end of the list
+const team_leaders = {
+	"name 1":false,
+	"name 2":false
+}
+
+//ToDo: Make 2 list, for our team and other for another extra team
+const aplicativos = {
+	"name 1":false,
+	"name 2":false,
+	"name 3":false
+}
+
 function check_new_users(){
 	let participants = document.getElementsByClassName(CONTAINER_PARTICIPANTS)
-	let new_participants = 0
+	let order_aplc = 0
 
-	//if there is a new participant add it at the end
 	for(let name of participants){
-		if(! participants_in_order.includes(name.textContent)){
-			participants_in_order.push(name.textContent)
-			//Test
-			new_participants ++
+
+		//Check team leaders
+		if(team_leaders[name.textContent] == false){
+			team_leaders[name.textContent] = true
+			continue
+		}else if(team_leaders[name.textContent] == true){
+			continue
+		}
+
+		//Check aplicativos members
+		if(aplc_members[name.textContent] == false){
+			aplc_members[name.textContent] = ++order_aplc
+			continue
+		}else if(aplc_members[name.textContent] == true){
+			continue
+		}
+
+		//Check team members
+		if(! team_members.includes(name.textContent)){
+			team_members.push(name.textContent)
 		}
 	}
-
-	//Test
-	//Print when the list changes
-	if(participants_in_order.length > original_size){
-		console.log(participants_in_order)
-		original_size += new_participants
-	}else{
-		console.log("No new users")
-	}
-	
 }
-let run_checking = window.setInterval(check_new_users, 1000)
+let run_checking = window.setInterval(check_new_users, TIME_REPLAY)
 function stop_checking(){
 	clearInterval(run_checking)
 }
@@ -41,8 +59,15 @@ let new_button = document.createElement("button")
 new_button.innerHTML = escapeHTMLPolicy.createHTML("Pasar Lista")
 new_button.onclick = function() {
 	let participants = ""
-	for(let i = 0; i < participants_in_order.length; i++){
-		participants = participants.concat(i+1, " - ", participants_in_order[i], "\n")
+	for(let i = 0; i < team_members.length; i++){
+		participants = participants.concat(i+1, " - ", team_members[i], "\n")
+	}
+
+	//Team leaders
+	for(let name in team_leaders){
+		if(team_leaders[name]){
+			participants = participants.concat("# - ", name, "\n")
+		}
 	}
 	navigator.clipboard.writeText(participants)
 	alert("Participantes copiados en el portapapeles")
